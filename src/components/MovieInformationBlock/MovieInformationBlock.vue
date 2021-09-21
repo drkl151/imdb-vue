55aa3dbc2aed6211f6719c9bb57bfc83d5752cec
drkl151 <B3drkl@gmail.com>
2021-09-21 21:13:47 +0300

    added component movie-info-block

---
+++ src/components/MovieInformationBlock/MovieInformationBlock.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="movie-information-block">
+    <div class="text-block">
+    <h2 class="text-block__movie-title">Assassin's Creed</h2>
+    <p class="text-block__movie-genre">Action, adventure, fantasy</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "MovieInformationBlock.scss";
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,23 +1,25 @@
 <template>
   <div>
     <Header />
+    <MovieInformationBlock />
   </div>
 </template>
 
 
 <script>
 import Header from "@/components/Header/Header";
+import MovieInformationBlock from "@/components/MovieInformationBlock/MovieInformationBlock";
 import { mapGetters, mapMutations } from "vuex";
 
 export default {
-  components: { Header },
+  components: { Header, MovieInformationBlock },
 
   methods: {
     ...mapMutations({
       changeFilm: "changeFilm",
     }),
   },
-  
+
   computed: {
     ...mapGetters({
       films: "film",
