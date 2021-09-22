7239c2221b9ad1afb39ef92bf107cdac17d7076e
drkl151 <B3drkl@gmail.com>
2021-09-22 15:47:38 +0300

    added hover over movie info block

--- src/components/MovieInformationBlock/MovieInformationBlock.vue
+++ src/components/MovieInformationBlock/MovieInformationBlock.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="movie-information-block">
+    <img
+      class="movie-information-block__movie-img"
+      src="../../assets/img-movie-information-block.png"
+      alt="movie-information"
+    />
+    <div class="information-block">
+      <div class="information-block__text-info">
+        <img
+        
+          src="../../assets/img/icon-information.svg"
+          alt="icon-information"
+        />
+        <p>Information</p>
+      </div>
+    </div>
     <div class="text-block">
-    <h2 class="text-block__movie-title">Assassin's Creed</h2>
-    <p class="text-block__movie-genre">Action, adventure, fantasy</p>
+      <h2 class="text-block__movie-title">Assassin's Creed</h2>
+      <p class="text-block__movie-genre">Action, adventure, fantasy</p>
     </div>
   </div>
 </template>
