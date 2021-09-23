5e8135302496cc4e62627367ae47f26aa8497425
drkl151 <B3drkl@gmail.com>
2021-09-23 19:19:00 +0300

    created side-header-component

---
+++ src/components/SideHeader/SideHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="side-header" :style="{ backgroundColor }">
+    <h3 class="side-header__text">{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    backgroundColor: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "SideHeader.scss";
+</style>
