b7c532e046abc96098a23d2f520ca0fceea955e8
drkl151 <B3drkl@gmail.com>
2021-09-30 16:32:36 +0300

    created a block of todays wallpaper

---
+++ src/containers/BlockTodaysWallpaper/BlockTodaysWallpaper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block-todays-wallpaper">
+    <h2 class="block-todays-wallpaper__title">Today's wallpapers</h2>
+    <TodaysWallpaper
+      v-for="todaysWallpaper in mainTodaysWalpaper"
+      :key="todaysWallpaper.id"
+      :backgroundImg="todaysWallpaper.backdrop_path"
+      :titleMovie="todaysWallpaper.title"
+    />
+  </div>
+</template>
+
+<script>
+import TodaysWallpaper from "@/components/TodaysWallpaper/TodaysWallpaper";
+import { mapGetters, mapActions } from "vuex";
+export default {
+  components: {
+    TodaysWallpaper,
+  },
+
+  methods: {
+    ...mapActions(["GET_TODAYS_WALLPAPERS_FROM_API"]),
+  },
+
+  computed: {
+    ...mapGetters({
+      todaysWallpapers: "todaysWallpapers",
+    }),
+
+    mainTodaysWalpaper() {
+      return this.todaysWallpapers.slice(0, 3);
+    },
+  },
+
+  mounted() {
+    this.GET_TODAYS_WALLPAPERS_FROM_API();
+  },
+};
+</script>
+
+<style lang="scss">
+@import "BlockTodaysWallpaper.scss";
+</style>
