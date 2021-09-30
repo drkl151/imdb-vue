0812b9da0c4febf2854e73161854224888727a2c
drkl151 <B3drkl@gmail.com>
2021-09-30 18:43:43 +0300

    created component signed-image

---
+++ src/components/SignedImage/SignedImage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="signed-image">
+    <div class="signed-image__img">
+      <img :src="img" :alt="title" />
+    </div>
+    <h2 class="signed-image__title">{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    img: {
+      type: String,
+      requared: true,
+    },
+    title: {
+      type: String,
+      requared: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "SignedImage.scss";
+</style>
